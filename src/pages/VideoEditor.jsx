2c9cb88b67e8631bd050368e62b3de1a5223dfa2
rosiest-77barbs <rosiest-77barbs@icloud.com>
2025-12-22 15:50:53 +0000

    File changes

--- src/pages/VideoEditor.jsx
+++ src/pages/VideoEditor.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider";
 import { Download, ChevronLeft } from "lucide-react";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "../utils";
