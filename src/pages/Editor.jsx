3886624068ccbd6d8f698d7f7e59ba54d51079ad
rosiest-77barbs <rosiest-77barbs@icloud.com>
2026-01-24 08:25:47 +0000

    File changes

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1244,7 +1244,7 @@ export default function Editor() {
         </Tabs>
       </motion.aside>
       
-      <main className="flex-1 flex flex-col order-1 lg:order-2 h-[60dvh] lg:h-auto relative min-h-0">
+      <main className="flex-1 flex flex-col relative min-h-0 h-full">
         <motion.div
           initial={{ y: -20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
